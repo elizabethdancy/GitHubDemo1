820d01b874857d50c6172d1dca4ce16b630a95da
dancye <elizabeth.dancy@sheridancollege.ca>
2021-05-18 12:29:05 -0400

    commit address

--- src/ca/sheridancollege/week1/softwarefundamentals/Student.java
+++ src/ca/sheridancollege/week1/softwarefundamentals/Student.java
@@ -9,6 +9,15 @@ public class Student
 {
     private String name;
     private String studentID;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public Student(String givenName, String givenID)
     {
